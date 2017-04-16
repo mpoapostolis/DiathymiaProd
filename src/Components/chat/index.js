7c9dbfc398a6029337aa65f201096e02b57c9e7a
mpoapostolis <mpoapostolis@gmail.com>
2017-04-17 01:00:22 +0300

    chat_v1

--- src/Components/chat/index.js
+++ src/Components/chat/index.js
@@ -11,14 +11,20 @@ const style = {
   chat: {
     borderRadius: '20px',
   },
-  heroAvatar:{
+  heroAvatar: {
     background: 'rgba(38, 121, 208, 0.23)',
   },
-  chip:{
-    borderRadius: '10px',
-    maxWidth: '300px',
-    margin: '0 15px 0 15px',
-    background: 'rgba(38, 121, 208, 0.12)',
+  userAvatar: {
+    background: 'rgba(9, 49, 92, 0.12)',
+  },
+  chip: (isHero) => {
+    return {
+      borderRadius: '30px',
+      width: '300px',
+      paddingLeft: '5px',
+      margin: '0 15px 0 15px',
+      background: isHero ? 'rgba(38, 121, 208, 0.12)' : 'rgba(9, 49, 92, 0.12)',
+    }
   }
 }
 
@@ -32,7 +38,7 @@ class Chat extends React.Component {
     const answers = ['ποτε', 'συχνα', 'συνέχεια']
     return <div className='mainHero'>
       <div className='answers'>
-        {answers.map( (answ, key) => <div onClick = {()=> {tmpArr.push(answ); this.setState({arr: tmpArr})}} key = {key} className ='answer'>{answ}</div> )}
+        {answers.map( (answ, key) => <div onClick = {()=> { tmpArr.push(answ); this.setState({arr: tmpArr}) }} key = {key} className='answer'> {answ} </div> )}
       </div>
       <div className = 'hero'>
         <img  className={`hero`} src={`/images/${hero}.png`} />
@@ -41,30 +47,27 @@ class Chat extends React.Component {
   }
 
   renChat(answ, hero, key){
-    return <div className='message hero'>
-      <Avatar style ={style.heroAvatar} src={`/images/${hero}.png`} />
-      <Chip style ={style.chip}>
-        <div style={{whiteSpace:'normal', padding:'10px'}}>
+    const isHero = key % 2 === 0
+    return <div key={key} className={`message ${isHero ? 'hero' : 'user' }`}>
+      {isHero ? <Avatar size={50} style ={style.heroAvatar} src={`/images/${hero}.png`} /> : null}
+      <Chip style ={style.chip(isHero)}>
+        <div style={{ whiteSpace:'normal', padding:'10px' }}>
           {answ}
         </div>
       </Chip>
+      {!isHero ? <Avatar size={50} style ={style.userAvatar} src={`/images/${hero}.png`} /> : null}
     </div>
   }
 
   render () {
-    console.log(this.state.arr);
     const { hero } = this.props
-    return (
-      <div className='chatContainer'>
+    const { arr } = this.state
+    return <div className='chatContainer'>
         {this.renHero(hero)}
         <div className='chatBox'>
-          {
-            this.state.arr.reverse().map((answ, key) => this.renChat(answ, hero, key)
-            )
-          }
+          <div>{ this.state.arr.map((answ, key) => this.renChat(answ, hero, key)) }</div>
         </div>
       </div>
-    )
   }
 }
 
